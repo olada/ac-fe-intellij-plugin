fee23e60b7f2d47c368f125b7b774b39bf2a5e09
dolah <david.olah@cofinpro.de>
2020-04-03 07:47:18 +0200

    resolve variable expressions in assignments (#74). For referencing github issues (could not reference previously due to downtime of github), following issues were dealt with in previous commits: #75, #77, #78, #79

--- src/main/java/de/cofinpro/intellij/acfeplugin/referencing/FormulaEngineIdentifierLiteralReference.java
+++ src/main/java/de/cofinpro/intellij/acfeplugin/referencing/FormulaEngineIdentifierLiteralReference.java
@@ -3,16 +3,14 @@ package de.cofinpro.intellij.acfeplugin.referencing;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiPolyVariantReference;
 import com.intellij.psi.ResolveResult;
-import com.intellij.psi.util.PsiTreeUtil;
-import de.cofinpro.intellij.acfeplugin.psi.FormulaEngineFunctionDefinition;
 import de.cofinpro.intellij.acfeplugin.psi.FormulaEngineIdentifierLiteral;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class FormulaEngineIdentifierLiteralReference extends FormulaEngineReference implements PsiPolyVariantReference {
-    private final PsiElement identifierLiteral;
+    private final FormulaEngineIdentifierLiteral identifierLiteral;
 
-    public FormulaEngineIdentifierLiteralReference(@NotNull PsiElement element) {
+    public FormulaEngineIdentifierLiteralReference(@NotNull FormulaEngineIdentifierLiteral element) {
         super(element);
         this.identifierLiteral = element;
     }
@@ -20,13 +18,8 @@ public class FormulaEngineIdentifierLiteralReference extends FormulaEngineRefere
     @Nullable
     @Override
     public PsiElement resolve() {
-        String identifierName = null;
-        if (identifierLiteral instanceof FormulaEngineIdentifierLiteral) {
-            identifierName = ((FormulaEngineIdentifierLiteral) identifierLiteral).getIdentifier().getText();
-        }
-        FormulaEngineFunctionDefinition surroundingFunction = PsiTreeUtil.getParentOfType(identifierLiteral, FormulaEngineFunctionDefinition.class);
-
-        return null;
+        String identifierName = identifierLiteral.getIdentifier().getText();
+        return findFirstDeclarationWalkingUp(identifierName, identifierLiteral).orElse(null);
     }
 
     @NotNull
